397f35fe38de0975dea2b39036844cf1a28e9bc3
Brian Haltiwanger <bhalt@bellsouth.net>
2019-02-11 15:20:59 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,8 +1,8 @@
 class Transfer
 
-  attr_reader :sender :receiver
+  attr_reader :sender, :receiver
 
-    def initialize(sender,receiver)
+    def initialize(sender, receiver)
       @name = name
       @receiver = receiver
     end
