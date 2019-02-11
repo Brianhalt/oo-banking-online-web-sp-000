af842cedafab6aff52addc0ccd03c0df6ef14f46
Brian Haltiwanger <bhalt@bellsouth.net>
2019-02-11 16:14:54 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -9,4 +9,17 @@ class Transfer
       @amount = amount
     end
 
+    def valid?
+      @sender.valid? && @receiver.valid?  ? true : false
+    end
+
+    def execute_transaction
+
+    end
+
+    def reverse_transaction
+
+
+    end
+    
 end
